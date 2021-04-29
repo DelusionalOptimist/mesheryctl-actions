54047f8b68ea7dc28c4288cb73652dd71e951ac2
Rudraksh Pareek <rudrakshpareek3601@gmail.com>
2021-04-30 03:02:05 +0530

    minor fixes

--- meshery.sh
+++ meshery.sh
@@ -4,8 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+cp ./mesheryctl /usr/local/bin
 git clone https://github.com/layer5io/meshery.git; cd meshery
 kubectl create namespace meshery
 helm install meshery --namespace meshery install/kubernetes/helm/meshery
 
-./mesheryctl system status
+mesheryctl
