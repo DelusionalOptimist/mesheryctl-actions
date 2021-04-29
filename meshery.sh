627c9460d0fb414cfcd1462ff59d1c73967e437d
Rudraksh Pareek <rudrakshpareek3601@gmail.com>
2021-04-30 03:19:16 +0530

    build mesheryctl on the runner

--- meshery.sh
+++ meshery.sh
@@ -4,9 +4,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-cp ./mesheryctl /usr/local/bin
 git clone https://github.com/layer5io/meshery.git; cd meshery
+git checkout v0.5.3
 kubectl create namespace meshery
 helm install meshery --namespace meshery install/kubernetes/helm/meshery
 
-mesheryctl
+cd ../mesheryctl; make make
+./mesheryctl
