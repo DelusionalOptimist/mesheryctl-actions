40159e78d3ca18135133751c566d7a36eb8ba711
Rudraksh Pareek <rudrakshpareek3601@gmail.com>
2021-04-30 19:11:12 +0530

    refactor
    Signed-off-by: Rudraksh Pareek <rudrakshpareek3601@gmail.com>

--- meshery.sh
+++ meshery.sh
@@ -4,10 +4,37 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-git clone https://github.com/layer5io/meshery.git; cd meshery
-git checkout v0.5.3
-kubectl create namespace meshery
-helm install meshery --namespace meshery install/kubernetes/helm/meshery
+main() {
 
-cd mesheryctl; make make
-./mesheryctl
+	clone_meshery
+	build_mesheryctl
+	install_meshery
+
+}
+
+# clone Meshery with the latest release tag
+clone_meshery() {
+	echo 'Cloning Meshery...'
+
+	MESHERY_VERSION=$(curl -L -s https://api.github.com/repos/layer5io/meshery/releases | \
+                  grep tag_name | sed "s/ *\"tag_name\": *\"\\(.*\\)\",*/\\1/" | \
+                  grep -v "rc\.[0-9]$"| head -n 1 )
+
+	git clone --depth=1 --branch $MESHERY_VERSION https://github.com/layer5io/meshery
+}
+
+# TODO: This step shouldn't be required once
+# https://github.com/layer5io/meshery/pull/2770 gets merged
+build_mesheryctl() {
+	echo 'Building Mesheryctl...'
+
+	make -C ./meshery/mesheryctl make
+	cp meshery/mesheryctl/mesheryctl .
+}
+
+install_meshery() {
+	echo 'Installing Meshery on namespace meshery...'
+
+	kubectl create namespace meshery
+	helm install meshery --namespace meshery meshery/install/kubernetes/helm/meshery
+}
