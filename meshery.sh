8851367f639b54d6c78725c525877877b823a896
Rudraksh Pareek <rudrakshpareek3601@gmail.com>
2021-06-30 18:29:35 +0530

    flatten kubeconfig
    Signed-off-by: Rudraksh Pareek <rudrakshpareek3601@gmail.com>

--- meshery.sh
+++ meshery.sh
@@ -15,13 +15,12 @@ main() {
 		echo "Cluster created successfully!"
 	fi
 
-	# get kubectl
-	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-
 	# TODO: config with the adapter specified by user
 	mkdir ~/.meshery
 	cp $SCRIPT_DIR/config.yaml ~/.meshery/config.yaml
 
+	kubectl config view --minify --flatten > ~/minified_config
+	mv ~/minified_config ~/.kube/config
 	# get mesheryctl
   curl -L https://git.io/meshery | PLATFORM=kubernetes bash -
 
