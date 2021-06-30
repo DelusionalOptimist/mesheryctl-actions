7ce920d08aff9eb1eb025c5566c2973f67fb9d14
Rudraksh Pareek <rudrakshpareek3601@gmail.com>
2021-06-30 16:10:11 +0530

    provision a cluster if none provided

--- meshery.sh
+++ meshery.sh
@@ -10,6 +10,16 @@ SCRIPT_DIR=$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}" || realpath "${BASH_S
 
 main() {
 
+	echo "Checking if a k8s cluster exits..."
+	kubectl config current-context
+	if [ $? -eq 0]
+	then
+		echo "Cluster found"
+	else
+		printf "Cluster not found. \nCreating one...\n"
+		create_k8s_cluster()
+		echo "Cluster created successfully!"
+
 	# get kubectl
 	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 	pwd
@@ -21,8 +31,17 @@ main() {
 	# get mesheryctl
   curl -L https://git.io/meshery | PLATFORM=kubernetes bash -
 
-	mesheryctl version
+	export TOKEN="${{ secrets.AUTH_TOKEN }}"
+	echo '{ "meshery-provider": "None", "token": null }' | jq '.token = ""' > ~/auth.json
 
+	mesheryctl perf apply --profile test --url https://google.com -t ~/auth.json
+
+}
+
+create_k8s_cluster() {
+	curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+	sudo install minikube-linux-amd64 /usr/local/bin/minikube
+	minikube start --driver=docker
 }
 
 main
