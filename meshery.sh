6ab21ce3240eecd84f07ec5c62650ad5882530f3
Rudraksh Pareek <rudrakshpareek3601@gmail.com>
2021-06-30 15:04:33 +0530

    rethinking

--- meshery.sh
+++ meshery.sh
@@ -4,53 +4,35 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}" || realpath "${BASH_SOURCE[0]}")")
+
 main() {
 
-	clone_meshery
-	install_mesheryctl
-	install_meshery
+	# get kubectl
+	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+	pwd
 
-}
+	# TODO: config with the adapter specified by user
+	mkdir ~/.meshery
+	cp $SCRIPT_DIR/config.yaml ~/.meshery
 
-# clone Meshery with the latest release tag
-clone_meshery() {
-	echo 'Cloning Meshery...'
+	# get mesheryctl
+  curl -L https://git.io/meshery | PLATFORM=docker bash -
 
-	MESHERY_VERSION=$(curl -L -s https://api.github.com/repos/layer5io/meshery/releases | \
-                  grep tag_name | sed "s/ *\"tag_name\": *\"\\(.*\\)\",*/\\1/" | \
-                  grep -v "rc\.[0-9]$"| head -n 1 )
 
-	git clone --depth=1 --branch $MESHERY_VERSION https://github.com/layer5io/meshery
-}
+	export KUBECONFIG=${HOME}/.kube/config
+	kubectl cluster-info --context kind-kind
 
-# TODO: This step shouldn't be required once
-# https://github.com/layer5io/meshery/pull/2770 gets merged
-install_mesheryctl() {
-	echo 'Building mesheryctl...'
-
-	make -C ./meshery/mesheryctl make
-
-	echo "Installing mesheryctl in /usr/local/bin..."
-
-	WHOAMI=$(whoami)
-	if mv ${PWD}/meshery/mesheryctl/mesheryctl /usr/local/bin/mesheryctl ; then
-	  echo "mesheryctl installed."
-	else
-	  if sudo mv ${PWD}/meshery/mesheryctl/mesheryctl /usr/local/bin/mesheryctl ; then
-	    echo "Permission Resolved: mesheryctl installed using sudo permissions."
-	  else
-	    echo "Cannot install mesheryctl. Check permissions of $WHOAMI for /usr/local/bin."
-	    exit 1
-	  fi
-	fi
-
-}
-
-install_meshery() {
-	echo 'Installing Meshery on namespace meshery...'
-
+	# install meshery
+	git clone https://github.com/layer5io/meshery.git; cd meshery
 	kubectl create namespace meshery
-	helm install meshery --namespace meshery meshery/install/kubernetes/helm/meshery
+	helm install meshery --namespace meshery install/kubernetes/helm/meshery
+	cd ../
+
+	mesheryctl version
+
 }
 
 main
