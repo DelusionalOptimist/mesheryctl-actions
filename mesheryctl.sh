0bbcd821b19c950416d2c34c329ed73aee5da276
Rudraksh Pareek <rudrakshpareek3601@gmail.com>
2021-07-12 13:02:29 +0530

    shell quoting issues

--- mesheryctl.sh
+++ mesheryctl.sh
@@ -13,8 +13,8 @@ main() {
 	parse_command_line "$@"
 	docker network connect bridge meshery_meshery_1
 	docker network connect minikube meshery_meshery_1
-	docker network connect minikube meshery_meshery-"'$service_mesh'"_1
-	docker network connect bridge meshery_meshery-"'$service_mesh'"_1
+	docker network connect minikube meshery_meshery-"$service_mesh"_1
+	docker network connect bridge meshery_meshery-"$service_mesh"_1
 
 	~/mesheryctl system config minikube -t ~/auth.json
 	echo $spec $service_mesh_adapter
